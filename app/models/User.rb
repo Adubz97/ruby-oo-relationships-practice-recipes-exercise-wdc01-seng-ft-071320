5eee376566786e11cebf8de9029be515d4d0c693
JinaStrickland <Jina30@gmail.com>
2020-07-22 16:56:44 -0400

    added more codes

--- app/models/User.rb
+++ app/models/User.rb
@@ -18,9 +18,9 @@ class User
         recipe_cards = RecipeCard.all.select do |recipe|
             recipe.user == self
         end
-            recipe_cards.map do |recipecard|
-                recipecard.recipe
-            end
+        recipe_cards.map do |recipecard|
+            recipecard.recipe
+        end
     end
 
     def add_recipe_card(recipe, date, rating)
@@ -30,9 +30,9 @@ class User
     # - `User#declare_allergy`
     # should accept an`Ingredient` instance as an argument, 
     # and create a new `Allergy` instance for this `User` and the given `Ingredient`
-        def declare_allergy
+        # def declare_allergy
 
-        end
+        # end
 
     # - `User#allergens`
     # should return all of the ingredients this user is allergic to
@@ -40,11 +40,27 @@ class User
 
     # - `User#top_three_recipes`
     # should return the top three highest rated recipes for this user.
+        def top_three_recipes
 
+        end
 
     # - `User#most_recent_recipe`
     # should return the recipe most recently added to the user's cookbook.
-
+        def most_recent_recipe
+            user_recipe = RecipeCard.all.select do |recipe|
+                recipe.user == self
+            end
+            recent = user_recipe.map do |recipe|
+                recipe.date
+            end
+            recent_recipe = user_recipe.select do |rec|
+                rec.date == recent.sort.last
+            end
+            recent_recipe.map do |rr|
+                rr.recipe
+            end
+            # binding.pry
+        end
 
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,5 @@
 require_relative '../config/environment.rb'
+require "date"
 
 #Users
 jina = User.new("Jina")
@@ -15,18 +16,27 @@ flour = Ingredient.new("Flour")
 coffee = Ingredient.new("Coffee")
 eggs =  Ingredient.new("Eggs")
 
+#date and time
+day1 = DateTime.new(2019,02,03)
+day2 = DateTime.new(2020,07,22)
+day3 = DateTime.new(2020,01,30)
+day4 = DateTime.new(2020,05,28)
+day5 = DateTime.new(2020,03,14)
+
 #Methods
-jina.add_recipe_card(tiramisu, "07/22", 6)
-jina.add_recipe_card(pasta, "06/02", 2)
-abdullah.add_recipe_card(pasta, "07/23",8)
-claudia.add_recipe_card(pizza, "07/24",7)
-claudia.add_recipe_card(pasta, "06/24",6)
+jina.add_recipe_card(tiramisu, day1, 5)
+jina.add_recipe_card(pasta, day2, 2)
+abdullah.add_recipe_card(pasta, day3, 8)
+claudia.add_recipe_card(pizza, day4, 7)
+claudia.add_recipe_card(pasta, day5, 6)
 
 pasta.add_ingredients(eggs)
 pasta.add_ingredients(flour)
 # print pasta.ingredients
 # print pasta.users
 # print jina.recipes
-p Recipe.most_popular
+# p Recipe.most_popular
+
+pp jina.most_recent_recipe
 
 # binding.pry
